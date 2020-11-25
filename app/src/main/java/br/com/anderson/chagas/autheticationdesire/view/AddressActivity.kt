584b5a8239adf1679a0201715571ba96465c8e8b
andersonchagas <anderson.1997reis@gmail.com>
2020-11-25 19:59:41 -0300

    layout da dados pessoais

---
+++ app/src/main/java/br/com/anderson/chagas/autheticationdesire/view/AddressActivity.kt
@@ -0,0 +1,16 @@
+package br.com.anderson.chagas.autheticationdesire.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.anderson.chagas.autheticationdesire.R
+
+class AddressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address)
+
+
+
+
+    }
+}
